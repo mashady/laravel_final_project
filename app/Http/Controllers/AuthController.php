b72ad812a2d967f8a90bfa131fc06ec298959dc9
Abdelwahab <abdelwahabsaeed415@gmail.com>
2025-05-27 18:34:10 +0300

    login end point was set suuccessfully

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -62,5 +62,28 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         //
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Invalid credentials.'], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login successful',
+            'data' => $user,
+            'token' => $token
+        ]);
     }
 }
