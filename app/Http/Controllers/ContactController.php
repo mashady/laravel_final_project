f93b183f439effa4247113068119ebe35aae6f6d
Abdelwahab <abdelwahabsaeed415@gmail.com>
2025-06-29 01:00:55 +0300

    Add contact us form successflully

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactMessageMail;
+
+class ContactController extends Controller
+{
+    public function send(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|min:2',
+            'email' => 'required|email',
+            'message' => 'required|string|min:10',
+        ]);
+
+        Mail::to('mostafamokna78@gmail.com')->send(new ContactMessageMail($validated));
+
+        return response()->json(['message' => 'Message sent successfully']);
+    }
+}
+
