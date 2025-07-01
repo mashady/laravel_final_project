e4a02512c89d95b5813c11ff4b35185a10de0823
Abdelwahab <abdelwahabsaeed415@gmail.com>
2025-07-02 02:29:57 +0300

    Verify status before login

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '1bb94e7bbede29d072e54650a8eabb95d098dcd9',
+        'reference' => '0a6fc8993e7281a79813c50ac38e671e277dbd3b',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -415,7 +415,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '1bb94e7bbede29d072e54650a8eabb95d098dcd9',
+            'reference' => '0a6fc8993e7281a79813c50ac38e671e277dbd3b',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
